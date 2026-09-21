9830bb171b4b81bbf1bc93584578435f28386658
Emerson Oliveira <emerson-developer@outlook.com>
2026-09-21 11:20:25 -0300

    adicionado um teste simples

--- src/test/java/br/com/hitbox/HitboxAdminApiApplicationTests.java
+++ src/test/java/br/com/hitbox/HitboxAdminApiApplicationTests.java
@@ -1,13 +1,17 @@
 package br.com.hitbox;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 class HitboxAdminApiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+
+    @Test
+    void teste_getEnv() {
+        System.out.println(System.getenv("URL"));
+        assert System.getenv("URL") != null;
+    }
 
 }
